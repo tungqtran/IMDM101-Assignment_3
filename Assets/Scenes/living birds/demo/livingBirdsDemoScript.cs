6691e2b0e027b45bcc7a8c561f75b0377d6577a4
Sienna Rader-Gowda <152990020+siennarg@users.noreply.github.com>
2025-11-10 15:50:37 -0500

    working_boat_scene w/o water

---
+++ Assets/Scenes/living birds/demo/livingBirdsDemoScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class lb_Bird : MonoBehaviour
+{
+    public float flySpeed = 5f;
+    public float turnSpeed = 60f;
+    public bool turning = false;
+
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        if (anim != null)
+        {
+            anim.SetBool("flying", true);
+        }
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * flySpeed * Time.deltaTime);
+
+        if (turning)
+        {
+            transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/lb_Boat.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class lb_Boat : MonoBehaviour
+{
+    public float driveSpeed = 5f;
+    public float turnSpeed = 60f;
+    public bool turning = false;
+
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        if (anim != null)
+        {
+            anim.SetBool("driving", true);
+        }
+        transform.rotation = Quaternion.Euler(0, 90, 0);
+    }
+
+    void Update()
+    {
+        transform.Translate(-transform.right * driveSpeed * Time.deltaTime);
+
+        if (turning)
+        {
+            transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime);
+        }
+    }
+}
