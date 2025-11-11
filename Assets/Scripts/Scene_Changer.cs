4acd30f6e2f6d99efdae6adf3b3cd102b6c576d0
Sienna Rader-Gowda <152990020+siennarg@users.noreply.github.com>
2025-11-11 10:35:27 -0500

    Final_Commit

--- Assets/Scripts/Scene_Changer.cs
+++ Assets/Scripts/Scene_Changer.cs
@@ -4,8 +4,17 @@ using UnityEngine.SceneManagement;
 
 public class Scene_Changer : MonoBehaviour
 {
+
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.CompareTag("Fishing_Boat"))
+    //    {
+    //        SceneManager.LoadScene("S2 - Underwater");
+    //    }
+    //}
     public float delay = 100f;
-    public string NewLevel= "S2 - Underwater";
+    public string NewLevel = "S2 - Underwater";
     void Start()
     {
         StartCoroutine(LoadLevelAfterDelay(delay));
--- Assets/Scripts/lb_Boat.cs
+++ Assets/Scripts/lb_Boat.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class lb_Boat : MonoBehaviour
 {
     public float driveSpeed = 5f;
     public float turnSpeed = 60f;
     public bool turning = false;
+    public string nextSceneName = "S2 - Underwater";
+    public float targetZ = 100f; // or any coordinate you want
+    public bool oneTimeOnly = true;
+
+    private bool sceneChanged = false;
 
     private Animator anim;
 
@@ -29,5 +35,7 @@ public class lb_Boat : MonoBehaviour
         {
             transform.Rotate(Vector3.up * turnInput * turnSpeed * Time.deltaTime);
         }
+
+
     }
 }
